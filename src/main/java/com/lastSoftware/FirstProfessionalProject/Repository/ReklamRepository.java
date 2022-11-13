00bc01a4f8832fbb1b0a27e57f10193f11952c0f
cihatcan.kaya <cihatcan.kaya@metasis.com.tr>
2022-11-13 19:49:50 +0300

    minibüs düzenle düzeltildi

--- src/main/java/com/lastSoftware/FirstProfessionalProject/Repository/ReklamRepository.java
+++ src/main/java/com/lastSoftware/FirstProfessionalProject/Repository/ReklamRepository.java
@@ -2,16 +2,8 @@ package com.lastSoftware.FirstProfessionalProject.Repository;
 
 import com.lastSoftware.FirstProfessionalProject.Entity.Reklam;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 
 public interface ReklamRepository extends JpaRepository<Reklam,Long> {
 
-    @Modifying
-    @Transactional
-    @Query(value = "SELECT * FROM reklamcilik.reklam r where r.id=:reklamId",nativeQuery = true)
-    Reklam findByIdForReklam(@Param("reklamId") Long reklamId);
 }
