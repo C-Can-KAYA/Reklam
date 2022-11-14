addd15c8810719ffd4eedc334a4849c720b4d577
cihatcan.kaya <cihatcan.kaya@metasis.com.tr>
2022-11-14 13:43:24 +0300

    minibüs düzenle düzeltildi

--- src/main/java/com/lastSoftware/FirstProfessionalProject/Repository/FirmaRepository.java
+++ src/main/java/com/lastSoftware/FirstProfessionalProject/Repository/FirmaRepository.java
@@ -2,7 +2,8 @@ package com.lastSoftware.FirstProfessionalProject.Repository;
 
 import com.lastSoftware.FirstProfessionalProject.Entity.Firma;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface FirmaRepository extends JpaRepository<Firma,Long> {
 }
--- src/main/java/com/lastSoftware/FirstProfessionalProject/Repository/ReklamRepository.java
+++ src/main/java/com/lastSoftware/FirstProfessionalProject/Repository/ReklamRepository.java
@@ -4,10 +4,11 @@ import com.lastSoftware.FirstProfessionalProject.Entity.Reklam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface ReklamRepository extends JpaRepository<Reklam,Long> {
 
     @Query(value = "SELECT * FROM reklam r where r.id=:id", nativeQuery = true)
