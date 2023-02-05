39d863f237c24909a7e3aa593b5e36e6c2efd2d8
cihatcan.kaya <kayacihat04@gmail.com>
2023-02-05 14:19:09 +0300

    mapper eklendi ve geri dönüş responseları değiştirildi

--- src/main/java/com/lastSoftware/FirstProfessionalProject/Service/Interface/ProjectMapper.java
+++ src/main/java/com/lastSoftware/FirstProfessionalProject/Service/Interface/ProjectMapper.java
@@ -9,16 +9,11 @@ import com.lastSoftware.FirstProfessionalProject.Web.Response.HatResponse;
 import com.lastSoftware.FirstProfessionalProject.Web.Response.IlResponse;
 import com.lastSoftware.FirstProfessionalProject.Web.Response.SoforResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProjectMapper {
-    @Mapping(source = "sicilNo", target = "sicilNo")
     FirmaResponse firmaBilgiToFirmaResponse(FirmaBilgi firmaBilgi);
-    @Mapping(source = "plakaKod", target = "plakaKod")
     HatResponse hatRequestToHatResponse(HatRequest hatRequest);
-    @Mapping(source = "plakaKod", target = "plakaKod")
     IlResponse ilRequestToIlResponse(IlRequest ilRequest);
-    @Mapping(source = "tckn", target = "tckn")
     SoforResponse soforBilgiToSoforResponse(SoforBilgi soforBilgi);
 }
